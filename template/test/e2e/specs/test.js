7abcf41da2d94993bf2d4ec2daaa6aaf6889793d
ragingwind <ragingwind@gmail.com>
2017-05-09 23:01:06 +0900

    Fixes test errors
    Fixes typo in tests
    Add a newline at bottom of App.vue
    Add a newline after import

--- template/test/e2e/specs/test.js
+++ template/test/e2e/specs/test.js
@@ -12,7 +12,7 @@ module.exports = {
       .url(devServer)
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
+      .assert.containsText('h1', 'Welcome to Your Vue.js PWA')
       .assert.elementCount('img', 1)
       .end(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
--- template/test/unit/specs/Hello.spec.js
+++ template/test/unit/specs/Hello.spec.js
@@ -6,6 +6,6 @@ describe('Hello.vue', () => {
     const Constructor = Vue.extend(Hello){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
     const vm = new Constructor().$mount(){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
     expect(vm.$el.querySelector('.hello h1').textContent)
-      .to.equal('Welcome to Your Vue.js App'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+      .to.equal('Welcome to Your Vue.js PWA'){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
   }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
