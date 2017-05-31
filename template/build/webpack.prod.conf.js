48b8cb6f3d88289de8574b02095cc3583e8fed81
li.li <li.li@ele.me>
2017-05-31 19:24:49 +0800

    update cacheId

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -98,7 +98,7 @@ var webpackConfig = merge(baseWebpackConfig, {
     ]),
     // service worker caching
     new SWPrecacheWebpackPlugin({
-      cacheId: 'my-vue-app',
+      cacheId: '{{ name }}',
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
