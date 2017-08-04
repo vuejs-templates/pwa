aadbe6f95dbe78850d7d6c48db40eea19c3743bf
anubhav7495 <anubhav.worklinux@gmail.com>
2017-08-04 08:44:50 +0530

    Typo Fix: load-minfied ==> load-minified

--- template/build/webpack.prod.conf.js
+++ template/build/webpack.prod.conf.js
@@ -10,7 +10,7 @@ var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
 var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin')
-var loadMinified = require('./load-minfied')
+var loadMinified = require('./load-minified')
 
 var env = {{#if_or unit e2e}}process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
