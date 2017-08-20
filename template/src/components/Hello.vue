129f863054c73142b4cb56327f8452db0c5295c7
Mark Pieszak <mpieszak84@gmail.com>
2017-08-20 15:54:48 -0400

    bug(chat): remove gitter chat link - update to community chat
     Changed to remove deprecated gitter and point to: http://chat.vuejs.org/

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -5,7 +5,7 @@
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank" rel="noopener">Gitter Chat</a></li>
+      <li><a href=" http://chat.vuejs.org/" target="_blank" rel="noopener">Vue Community Chat</a></li>
       <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
       <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank" rel="noopener">Docs for This Template</a></li>
     </ul>
