d9ef3161196275fc395a734707a8c5c9fee775fd
Eduardo Peredo <jrperedo@gmail.com>
2017-09-10 03:00:13 -0500

    Fixed bad attributes to style tag inside component

--- template/src/components/Home.vue
+++ template/src/components/Home.vue
@@ -16,7 +16,7 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped="scss">
+<style lang="scss" scoped>
 h1 {
 	color: map-get($colors, red);
 	font-size: em(32);
