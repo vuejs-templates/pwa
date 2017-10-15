08d80203604a8cda2edbb4b452ce8af13485e886
Eduardo Peredo <jrperedo@gmail.com>
2017-10-15 10:46:56 -0500

    fix(Indent): Updated indentation from spaces to tabs :speak_no_evil:

--- template/src/router/index.js
+++ template/src/router/index.js
@@ -5,11 +5,11 @@ import Home from '@/components/Home'{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 Vue.use(Router){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Hello{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-    }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
-  ]{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+	routes: [
+		{
+			path: '/',
+			name: 'Home',
+			component: Home{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+		}{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+	]{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
 }){{#if_eq lintConfig "airbnb"}};{{/if_eq}}
